e670bc490e631a0d0c3b912eba861afa9cb730f0
Angel_Lee <angel8200531@gmail.com>
2021-06-18 00:01:11 +0800

    Create 2 objects: FoodTypes, Food

--- veg_market/models.py
+++ veg_market/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class FoodTypes(models.Model):
+	types = models.CharField(max_length=10)
+	slug = models.SlugField()
+
+	def __str__(self):
+		return self.types
+
+
+class Food(models.Model):
+	name = models.CharField(max_length=10, unique = True)
+	slug = models.SlugField()
+	quantity = models.IntegerField()
+	note = models.TextField()
+	foodtypes = models.ForeignKey(FoodTypes, on_delete = models.CASCADE) 
+	#on_delete表示當對應的FoodTypes被刪除時，Food也一起刪除(CASCADE)
+
+	def __str__(self):
+		return "<{}>{}({}):{}".format(self.foodtypes, self.name, self.quantity, self.note)
+
+		
