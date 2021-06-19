ac493e1adf13bdf2e5e745c4b0d23162959a3d90
Angel_Lee <angel8200531@gmail.com>
2021-06-19 23:20:07 +0800

    Update data to veg_market.html

--- veg_market/models.py
+++ veg_market/models.py
@@ -12,11 +12,12 @@ class Food(models.Model):
 	name = models.CharField(max_length=10, unique = True)
 	slug = models.SlugField()
 	quantity = models.IntegerField()
+	price = models.IntegerField()
 	note = models.TextField()
 	foodtypes = models.ForeignKey(FoodTypes, on_delete = models.CASCADE) 
 	#on_delete表示當對應的FoodTypes被刪除時，Food也一起刪除(CASCADE)
 
 	def __str__(self):
-		return "<{}>{}({}):{}".format(self.foodtypes, self.name, self.quantity, self.note)
+		return "<{}>{}(${}),In stock:{} #{}".format(self.foodtypes, self.name, self.price, self.quantity, self.note)
 
 		
--- veg_market/views.py
+++ veg_market/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from veg_market.models import FoodTypes, Food
 
 def homepage(request):
 	return render(request, 'homepage.html')
 
 def veg_market(request):
-	return render(request, 'veg_market.html')
+	foodtypes = FoodTypes.objects.all()
+	food = Food.objects.all()
+	return render(request, 'veg_market.html', {'foodtypes':foodtypes, 'food':food})
